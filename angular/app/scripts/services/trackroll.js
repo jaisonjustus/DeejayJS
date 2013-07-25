bd9169aaef647f8981abdecf29b1d44994893d00
Jaison Justus <jaison.justus.lp@gmail.com>
2013-07-25 10:49:37 +0530

    Add track loading service.
    Add track service to load the sounds from the resource folder and provide
    the audio buffer array.

---
+++ angular/app/scripts/services/trackroll.js
@@ -0,0 +1,52 @@
+'use strict'
+
+angular.module('Deejay')
+
+	.service('Track', function($q, $http)	{
+		
+		var _tracksList, _loadTrack, _audioBuffers = {}, _iterateAndLoadTrackList;
+
+		_tracksList = {
+			takabeat : 'takabeat.wav',
+			rainyday : 'rainyday.wav'
+		};
+
+		_iterateAndLoadTrackList = function()	{
+			var trackListCount = 0,
+					trackLoadCounter = 0,
+					defered = null;
+
+			defered = $q.defer();
+
+			for(var track in _tracksList) {
+				var url = '/sounds/loops/' + _tracksList[track];
+
+				trackListCount++;
+
+	      $http({ method : 'GET', url : url, identifier : track, })
+	      	.success(function(data, status, headers, config)	{
+	      		_audioBuffers[config.identifier] = data;
+	      		
+	      		trackLoadCounter++;
+	      		if(trackListCount === trackLoadCounter)	{ defered.resolve(_audioBuffers); }
+	      });
+	    }
+
+	    return defered.promise;
+		}
+
+		return	{
+			getTrackList : function()	{
+				return _tracksList;
+			},
+
+			getTracks : function()	{
+				return _iterateAndLoadTrackList();
+			},
+
+			getAudioBuffers : function()	{
+				return _audioBuffers;
+			}
+		}
+
+	});
